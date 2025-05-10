988bc4a792827c9b3382104b36062a0da6273836
notlre <notagreentree.purple@gmail.com>
2025-05-10 20:19:20 +0800

    base code

--- musical_instruments.py
+++ musical_instruments.py
@@ -1,1 +1,28 @@
-#test code by justine
+from abc import ABC, abstractmethod
+
+# Abstract Base Class (Parent)
+class Musical_Instrument:
+    def __init__(self, name, instrument_type, brand):
+        self.name = name
+        self.instrument_type = instrument_type
+        self.brand = brand
+
+    @abstractmethod
+    def play_sound(self):
+        pass
+    @abstractmethod
+    def tune(self):
+        pass
+
+# Child Class
+class Guitar(Musical_Instrument):
+    
+    def play_sound(self):
+        print(f"{self.name} is strumming nonchalantly.")
+    
+    def tune(self):
+        print(f"Tuning {self.name}")
+    
+guitar = Guitar("Electric Guitar", "String", "Yamaha")
+
+guitar.play_sound()
